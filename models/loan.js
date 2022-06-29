6a817d5c317a1a9a76cad8dce694a8561471a548
Rebecca Wood <Rebecca.Wood@softwire.com>
2022-06-29 14:08:37 +0100

    added table sequelize classes

--- models/loan.js
+++ models/loan.js
@@ -1,5 +1,10 @@
-export class Loan {
-    constructor() {
+import { Sequelize, Model, DataTypes } from 'sequelize';
 
-    }
-}
+const sequelize = new Sequelize('sqlite::memory:');
+const Loan = sequelize.define('User', {
+    loanid: DataTypes.INTEGER,
+    duedate: DataTypes.DATE,
+    loanstatus: DataTypes.STRING(50),
+    barcodenumber: DataTypes.STRING(13),
+    username: DataTypes.STRING(50)
+});
